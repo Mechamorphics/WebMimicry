d11527cd0d0b449cf351d50b1bb7f85598388c86
Tankiso Montse <tmontse16@gmail.com>
2025-05-06 00:00:29 +0200

    added function that allows you to select and deselect filters when clicking on them

--- oh.studio/js/script.js
+++ oh.studio/js/script.js
@@ -3,3 +3,13 @@ function onSelectTab(clickedTab) {
   tabs.forEach(item => item.classList.remove('active-tab'));
   clickedTab.classList.add('active-tab');
 }
+
+function onFilterSelect(clickedFilter) {
+  const filters = document.querySelectorAll('.filter-option');
+
+  if (clickedFilter.classList.contains('active-filter')) {
+    clickedFilter.classList.remove('active-filter');
+  } else {
+    clickedFilter.classList.add('active-filter');
+  }
+}
